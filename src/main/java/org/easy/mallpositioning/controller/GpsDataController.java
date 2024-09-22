159e0b2a99550d5a94d6123ef61e389b6a5dc907
EvoltoStart <2171290799@qq.com>
2024-09-22 21:18:06 +0800

    ...

--- src/main/java/org/easy/mallpositioning/controller/GpsDataController.java
+++ src/main/java/org/easy/mallpositioning/controller/GpsDataController.java
@@ -1,5 +1,7 @@
 package org.easy.mallpositioning.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 
@@ -12,7 +14,16 @@ import org.springframework.stereotype.Controller;
  * @since 2024-09-21
  */
 @Controller
-@RequestMapping("/mallpositioning/gpsData")
+@RequestMapping("/api")
 public class GpsDataController {
+    @PostMapping("/location")
+    public String location(@RequestBody String data) {
+        System.out.println("========="+data);
+        return "success";
+    }
+
+
+
+
 
 }
