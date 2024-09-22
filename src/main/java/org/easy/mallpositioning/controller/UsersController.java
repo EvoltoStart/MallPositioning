9dfc881d3410379448a26fd99ef06f4b1dda2037
EvoltoStart <2171290799@qq.com>
2024-09-22 21:59:47 +0800

    ...

--- src/main/java/org/easy/mallpositioning/controller/UsersController.java
+++ src/main/java/org/easy/mallpositioning/controller/UsersController.java
@@ -1,5 +1,11 @@
 package org.easy.mallpositioning.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.easy.mallpositioning.entity.Users;
+import org.easy.mallpositioning.service.IUsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 
@@ -12,7 +18,17 @@ import org.springframework.stereotype.Controller;
  * @since 2024-09-21
  */
 @Controller
+@Api("用户")
 @RequestMapping("/mallpositioning/users")
 public class UsersController {
 
+    @Autowired
+    private IUsersService usersService;
+    @ApiOperation("xxx")
+    @GetMapping("/index")
+    public String index(){
+      Users users= usersService.getById(1);
+      return users.toString();
+
+    }
 }
